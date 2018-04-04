acdc1479d4d39022bae95afbc293d7da7c42578a
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2018-04-04 10:51:50 -0400

    Happy and Sad emojis

--- HappyOrSad/main.swift
+++ HappyOrSad/main.swift
@@ -11,7 +11,49 @@ import Foundation
 // Get the user input
 var rawInput = readLine()
 
+// Make sure the input was given ( create a String from the String?)
+guard let input = rawInput else {
+    // Error
+    exit(9)
+}
+
 // Print out the input provided
 print("You said:")
-print(rawInput)
+print(rawInput) // optional String
+print(input) // non-optional
+
+// Process - inspect each character
+var happyCount = 0
+var sadCount = 0
+for individualCharacter in input {
+    
+    // Debug
+    print(individualCharacter)
+    
+    // Categorize the character
+    if individualCharacter == "😃" || individualCharacter == "☺️" || individualCharacter == "🙂" || individualCharacter == "😄" {
+        // Track a happy
+        happyCount += 1
+    } else if individualCharacter == "🙁"  || individualCharacter == "☹️" || individualCharacter == "😔" || individualCharacter == "😕"  {
+        // Track a sad
+        sadCount += 1
+    }
+    
+    
+}
+    
+
+
+// Output
+// Tell the user "happy", "sad", or "unsure" based o counts above
+
+if happyCount == sadCount {
+    print("unsure")
+} else if happyCount > sadCount {
+    print("happy")
+} else if sadCount > happyCount {
+    print("sad")
+}
+
+
 
